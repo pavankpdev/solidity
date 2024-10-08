ed19823a45a326bb6b86c7a669662319f0b05e83
pavanleton <pavankumarkpkp@gmail.com>
2024-10-08 08:57:38 +0530

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
 import 'solidity-coverage';
 import "@nomiclabs/hardhat-solhint";
+// import 'hardhat-deploy';
 
 import "./tasks/add-claim.task";
 import "./tasks/add-key.task";
@@ -14,10 +15,16 @@ import "./tasks/revoke.task";
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com/v1/9cd3d6ce21f0a25bb8f33504a1820d616f700d24',
-      accounts: ["1d79b7c95d2456a55f55a0e17f856412637fa6b3c332fa557ce2c8a89139ec74"],
-    }
+    hardhat: {
+      accounts: [{
+        privateKey: "0x1538d25d04e53f4bc73e1aef6dff73d4e34952440385f1cb93d3515e50099e10",
+        balance: "10000000000000000000000"
+      }],
+    },
+    local: {
+      url: 'HTTP://127.0.0.1:7545',
+      accounts: ["0x1538d25d04e53f4bc73e1aef6dff73d4e34952440385f1cb93d3515e50099e10"],
+    },
   }
 };
 
